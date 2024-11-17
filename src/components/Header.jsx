b9a4eecdd34a52845b40cc5ffce4d9870d8539a0
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-17 10:55:14 +0100

    :lipstick: give some air to the header to breath

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between text-left border-gray-900">
+    <header className="flex p-3 items-center justify-between text-left border-gray-900">
       <Logo />
       <div>CV Builder v1.1</div>
     </header>
